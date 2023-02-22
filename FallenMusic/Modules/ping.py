5fa8c1fc84e833241e101866c681f86fbf2f3058
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-02-22 21:05:08 +0530

    MU

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -59,8 +59,8 @@ async def ping_fallen(_, message: Message):
                 [
                     InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
-                        "✨ sᴏᴜʀᴄᴇ ✨",
-                        url="https://github.com/AnonymousX1025/FallenMusic",
+                        "✨ WD Zone ✨",
+                        url="https://t.me/Opleech",
                     ),
                 ],
             ]
