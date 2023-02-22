e9da81dd744274620dc55880fca339045bc1c787
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-02-22 23:37:38 +0530

    op

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/08f1061f32fc52b45a722.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/08f1061f32fc52b45a722.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/WOODcraft_Mirror_Topic")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Opleech")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
