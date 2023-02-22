5291859f7c7588a9c8365390fb99f11c4017aeda
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-02-23 00:01:14 +0530

    OP

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -45,11 +45,11 @@ buttons = InlineKeyboardMarkup(
 pm_buttons = [
     [
         InlineKeyboardButton(
-            text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+            text="🌹 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🌹",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         )
     ],
-    [InlineKeyboardButton(text="ʜᴇʟᴩ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="fallen_help")],
+    [InlineKeyboardButton(text="❣️ ʜᴇʟᴩ & ᴄᴏᴍᴍᴀɴᴅs ❣️", callback_data="fallen_help")],
     [
         InlineKeyboardButton(text="❄ ᴄʜᴀɴɴᴇʟ ❄", url=config.SUPPORT_CHANNEL),
         InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=config.SUPPORT_CHAT),
@@ -66,7 +66,7 @@ pm_buttons = [
 gp_buttons = [
     [
         InlineKeyboardButton(
-            text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+            text="🌹 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🌹",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         )
     ],
@@ -86,17 +86,17 @@ gp_buttons = [
 helpmenu = [
     [
         InlineKeyboardButton(
-            text="ᴇᴠᴇʀʏᴏɴᴇ",
+            text="❣️ ᴇᴠᴇʀʏᴏɴᴇ ❣️",
             callback_data="fallen_cb help",
         )
     ],
     [
-        InlineKeyboardButton(text="sᴜᴅᴏ", callback_data="fallen_cb sudo"),
-        InlineKeyboardButton(text="ᴏᴡɴᴇʀ", callback_data="fallen_cb owner"),
+        InlineKeyboardButton(text="🍇sᴜᴅᴏ", callback_data="fallen_cb sudo"),
+        InlineKeyboardButton(text="ᴏᴡɴᴇʀ🍓", callback_data="fallen_cb owner"),
     ],
     [
-        InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="fallen_home"),
-        InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
+        InlineKeyboardButton(text="🍓ʙᴀᴄᴋ", callback_data="fallen_home"),
+        InlineKeyboardButton(text="ᴄʟᴏsᴇ🍇", callback_data="close"),
     ],
 ]
 
@@ -104,7 +104,7 @@ helpmenu = [
 help_back = [
     [InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=config.SUPPORT_CHAT)],
     [
-        InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="fallen_help"),
-        InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
+        InlineKeyboardButton(text="🍓ʙᴀᴄᴋ", callback_data="fallen_help"),
+        InlineKeyboardButton(text="ᴄʟᴏsᴇ🍇", callback_data="close"),
     ],
 ]
